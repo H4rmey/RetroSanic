828d376eb35fcaa72fecf0ecd27b2443a0cda68e
H4rmey <hammiekaaz@gmail.com>
2020-07-14 10:54:04 +0200

    Update - Fixed previous bugs with jonachan

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -27,14 +27,14 @@ public class Player : MonoBehaviour
     public float    jumpAccend = .4f;
     public float    smoothingAirborneX = .4f;
     public float    smoothingGroundedX = .1f;
-    public float    smoothingAriborneY = .1f;
+    public float    smoothingAirborneY = .1f;
 
     [Header("Movement")]
     public float    moveSpeed = 10;
     private bool    isSliding = false;
 
     [Header("WallJumping")]
-    public float        wallSlideSpeedMax   = 3;
+    public float        wallSlideSpeed   = 3;
     private bool        isWallSliding       = false;
     public float        slideSmoothing      = 2;
     private float       slideXSmoothing;
@@ -66,25 +66,10 @@ public class Player : MonoBehaviour
 
         //smooth the input x speed
         isSliding = input.pDown;
-        if (!isSliding)
-        {
-            slideXSmoothing = 0;
-
-            float targetVelocityX = moveSpeed * input.pHorizontal;
-            if (IsAirborne())
-                velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, smoothingAirborneX);
-            else
-                velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, smoothingGroundedX);
-        }
-        else
-        {
-            velocity.x = Mathf.SmoothDamp(velocity.x, 0, ref slideXSmoothing, slideSmoothing);
-        }
+        Movement();
 
         //make function
-        if (input.pJump          && 
-            !isWallSliding              &&
-            controller.collisions.below)
+        if (input.pJump && !isWallSliding && controller.collisions.below)
         {
             velocity.y = jumpForce;
         }
@@ -97,27 +82,57 @@ public class Player : MonoBehaviour
 
         if (isWallSliding)
         {
-            if (input.pDown                 || 
-                controller.collisions.below ||
-                (!controller.collisions.left && !controller.collisions.right))
-            {
-                isWallSliding = false;
-            }
-            else if (input.pJump)
-            {
-                isWallSliding = false;
-
-                velocity.x = jumpForce * -controller.collisions.horizontal;
-                velocity.y = jumpForce;
-            }
-
-            velocity.y = Mathf.SmoothDamp(velocity.y, (input.pLeft) ? 0.0f : -wallSlideSpeedMax, ref velocityYSmoothing, smoothingAriborneY);
+            WallSliding();
         }
+               
+
+        if (controller.collisions.above)
+            velocity.y = 0;
 
         if (!isWallSliding)
             velocity.y += gravity * Time.deltaTime;
-        //Debug.Log(velocity.y);
+
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Movement()
+    {
+        if (!isSliding)
+        {
+            slideXSmoothing = 0;
+
+            float targetVelocityX = moveSpeed * input.pHorizontal;
+            if (IsAirborne())
+            {
+                velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, smoothingAirborneX);
+            }
+            else
+            {
+                velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, smoothingGroundedX);
+            }
+        }
+        else
+        {
+            velocity.x = Mathf.SmoothDamp(velocity.x, 0, ref slideXSmoothing, slideSmoothing);
+        }
+    } 
+
+    private void WallSliding()
+    {
+        if (input.pDown || IsAirborne())
+        {
+            isWallSliding = false;
+        }
+        else if (input.pJump)
+        {
+            isWallSliding = false;
+
+            velocity.x = jumpForce * -controller.collisions.horizontal;
+            velocity.y = jumpForce;
+        }
+
+        float targetVelocityY = (input.pLeft || input.pRight) ? 0.0f : -wallSlideSpeed;
+        velocity.y = Mathf.SmoothDamp(velocity.y, targetVelocityY, ref velocityYSmoothing, smoothingAirborneY);
     }
 
     private bool IsAirborne()
---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,232 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : RaycastController
+{
+    private float           maxClimbAngle = 45;
+    private float           maxDescendAngle = 45;
+    public CollisionsInfo   collisions;
+    [HideInInspector]
+    public InputHandler     inputHandler;
+
+    public override void Start()
+    {
+        base.Start();
+        collisions.faceDir = 1;
+
+        inputHandler = GameObject.Find("SGameManager").GetComponent<InputHandler>();
+    }
+
+    public void Move(Vector3 velocity, bool isStandingOnPlatform = false)
+    {
+        UpdateRaycastOrigins();
+        collisions.Reset();
+        collisions.velocityOld = velocity;
+
+        if (velocity.x > -0.001 && velocity.x < 0.001)
+            velocity.x = 0;
+
+        if (velocity.x != 0)
+            collisions.faceDir = (int)Mathf.Sign(velocity.x);
+
+        if (velocity.y <= 0)
+            DescendSlope(ref velocity);
+
+        HoriCollisions(ref velocity);
+
+        if (velocity.y != 0)
+            VertCollisions(ref velocity);
+
+        transform.Translate(velocity);
+
+        if (isStandingOnPlatform)
+        {
+            collisions.below = true;
+            collisions.vertical = -1;
+        }
+
+        //Debug.Log(velocity.x);
+    }
+
+    void HoriCollisions(ref Vector3 velocity)
+    {
+        float dirX = collisions.faceDir;
+        float raylen = Mathf.Abs(velocity.x) + skinWidth;
+
+        if (Mathf.Abs(velocity.x) < skinWidth)
+        {
+            raylen = 2 * skinWidth;
+        }
+
+        for (int i = 0; i < vertRayCount; i++)
+        {
+            Vector2 rayOrigin = (dirX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horiRaySpacing * i);
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * dirX, raylen, colMask);
+            Debug.DrawRay(rayOrigin, Vector2.right * dirX * raylen, Color.red);
+
+            if (hit)
+            {
+                if (hit.distance == 0)
+                {
+                    continue;
+                }
+
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if (i==0 && slopeAngle <= maxClimbAngle)
+                {
+                    if (collisions.descendingSlope)
+                    {
+                        collisions.descendingSlope = false;
+                        velocity = collisions.velocityOld;
+                    }
+                    float distanceToSlopeStart = 0;
+                    if (slopeAngle != collisions.slopeAngleOld)
+                    {
+                        distanceToSlopeStart = hit.distance - skinWidth;
+                        velocity.x -= distanceToSlopeStart * dirX;
+                    }
+                    ClimbSlope(ref velocity, slopeAngle);
+                    velocity.x += distanceToSlopeStart * dirX;
+                }
+
+                if (!collisions.climbinSlope || slopeAngle > maxClimbAngle)
+                {
+                    velocity.x = (hit.distance - skinWidth) * dirX;
+                    raylen = hit.distance;
+
+                    if (collisions.climbinSlope)
+                    {
+                        velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+                    }
+
+                    collisions.left         = dirX == -1;
+                    collisions.right        = dirX == 1;
+                    collisions.horizontal   = (int)dirX;
+                }
+            }
+        }
+    }
+
+    void VertCollisions(ref Vector3 velocity)
+    {
+        float dirY = Mathf.Sign(velocity.y);
+        float raylen = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < vertRayCount; i++)
+        {
+            Vector2 rayOrigin = (dirY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (vertRaySpacing * i + velocity.x);
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * dirY, raylen, colMask);
+            Debug.DrawRay(rayOrigin, Vector2.up * dirY * raylen, Color.red);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * dirY;
+                raylen = hit.distance;
+
+                if (collisions.climbinSlope)
+                {
+                    velocity.x = Mathf.Abs(velocity.y) / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+                }
+
+                collisions.below    = dirY == -1;
+                collisions.above    = dirY == 1;
+                collisions.vertical = (int)dirY;
+            }
+        }
+
+        if (collisions.climbinSlope)
+        {
+            float dirX = Mathf.Sign(velocity.x);
+            raylen = Mathf.Abs(velocity.x) + skinWidth;
+            Vector2 rayOrigin = ((dirX == 1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight) + Vector2.up * velocity.y;
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * dirX, raylen, colMask);
+            if (hit)
+            {
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if (slopeAngle != collisions.slopeAngle)
+                {
+                    velocity.x = (hit.distance - skinWidth) * dirX;
+                    collisions.slopeAngle = slopeAngle;
+                }
+            }
+        }
+    }
+
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle)
+    {
+        float moveDist = Mathf.Abs(velocity.x);
+        float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDist;
+
+        if (velocity.y <= climbVelocityY)
+        {
+            velocity.y = climbVelocityY;
+            velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDist * Mathf.Sign(velocity.x);
+
+            collisions.below        = true;
+            collisions.vertical     = -1;
+            collisions.climbinSlope = true;
+            collisions.slopeAngle = slopeAngle;
+        }
+    }
+
+    void DescendSlope(ref Vector3 velocity)
+    {
+        float dirX = Mathf.Sign(velocity.x);
+
+        Vector2 rayOrigin = ((dirX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft);
+
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity, colMask);
+        if (hit)
+        {
+            float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+            if (slopeAngle != 0 && slopeAngle <= maxDescendAngle)
+            {
+                if (Mathf.Sign(hit.normal.x) == dirX)
+                {
+                    if (hit.distance - skinWidth <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x))
+                    {
+                        float moveDist = Mathf.Abs(velocity.x);
+                        float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDist;
+                        velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDist * Mathf.Sign(velocity.x);
+                        velocity.y -= descendVelocityY;
+
+                        collisions.below            = true;
+                        collisions.vertical         = -1;
+                        collisions.descendingSlope  = true;
+                        collisions.slopeAngle       = slopeAngle;
+                    }
+                }
+            }
+        }
+    }
+
+    public struct CollisionsInfo
+    {
+        public bool above, below;
+        public bool left, right;
+        public int horizontal, vertical;
+
+        public bool climbinSlope;
+        public bool descendingSlope;
+        public float slopeAngle, slopeAngleOld;
+        public Vector3 velocityOld;
+        public int faceDir;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+            climbinSlope = false;
+            descendingSlope = false;
+
+            slopeAngleOld = slopeAngle;
+            slopeAngle = 0;
+        }
+    }
+}
